d4302fbdea0202e07133f2f70ca344630b61317d
Rigelyon <pandudwi120@gmail.com>
2026-02-10 16:40:39 +0700

    Add caelestia dependencies install script

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,15 @@
 
 set -ouex pipefail
 
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+dnf copr enable -y errornointernet/quickshell
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,14 +19,62 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+PACKAGES=(
+    # Core Hyprland & Portals
+    hyprland
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
+    hyprpicker
+    
+    # Clipboard & Files
+    wl-clipboard
+    cliphist
+    inotify-tools
+    trash-cli
+    jq
+    
+    # Audio & Media
+    wireplumber
+    pipewire-utils
+    
+    # Shell & Terminal
+    foot
+    fish
+    starship
+    fastfetch
+    eza
+    btop
+    
+    # Theme & Appearance
+    adw-gtk3-theme
+    papirus-icon-theme
+    qt5ct
+    qt6ct
+    
+    # Caelestia Specifics
+    quickshell
+    socat           # Dibutuhkan untuk komunikasi socket
+    python3-pip     # Untuk install script python nanti
+    git
+    wget
+    unzip
+)
+
+rpm-ostree install "${PACKAGES[@]}"
+
+FONT_DIR="/usr/share/fonts/jetbrains-nerd"
+mkdir -p "$FONT_DIR"
+wget -P "$FONT_DIR" https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip -o "$FONT_DIR/JetBrainsMono.zip" -d "$FONT_DIR"
+rm "$FONT_DIR/JetBrainsMono.zip"
+
+# Update cache font sistem
+fc-cache -fv
+
+rpm-ostree cleanup -m
+
+dnf5 install -y tmux 
 
 #### Example for enabling a System Unit File
 
