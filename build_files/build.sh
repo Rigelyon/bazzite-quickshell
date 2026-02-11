7081ef6bb104907bd1be6aa39a4e452b8113988a
Rigelyon <pandudwi120@gmail.com>
2026-02-11 18:25:13 +0700

    Add caelestia-cli dependencies

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,8 @@ dnf copr enable -y solopasha/hyprland
 
 dnf copr enable atim/starship
 
+dnf copr enable brycensranch/gpu-screen-recorder-git
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -22,61 +24,87 @@ dnf copr enable atim/starship
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
 
-# Dependencies for Caelestia
+dnf5 install -y tmux 
+
+brew install sass/sass/sass # Caelestia cli dependency
+
 PACKAGES=(
-    # Core Hyprland & Portals
+    ## Caelestia dependencies
     hyprland
     xdg-desktop-portal-hyprland
     xdg-desktop-portal-gtk
     hyprpicker
-    
-    # Clipboard & Files
     wl-clipboard
     cliphist
     inotify-tools
     trash-cli
     jq
-    
-    # Audio & Media
     wireplumber
     pipewire-utils
-    
-    # Shell & Terminal
     foot
     fish
     fastfetch
     btop
-    
-    # Theme & Appearance
     adw-gtk3-theme
     papirus-icon-theme
     qt5ct
     qt6ct
-    
-    # Caelestia Specifics
     quickshell
-    socat           # Dibutuhkan untuk komunikasi socket
-    python3-pip     # Untuk install script python nanti
+    socat
+    python3-pip
     git
     wget
     unzip
+
+    ## Caelestia Cli dependencies
+    libnotify
+    swappy
+    grim
+    slurp
+    gpu-screen-recorder-ui
+    glib2
+    fuzzel
+    python-build
+    python-installer
+    hatch
+    python-hatch-vcs
 )
 
 rpm-ostree install "${PACKAGES[@]}"
 
-# Install eza from repo
+echo "Installing App2Unit"
+git clone --depth=1 https://github.com/Vladimir-csp/app2unit.git /tmp/app2unit
+cd /tmp/app2unit
+make
+make install PREFIX=/usr
+cd /
+rm -rf /tmp/app2unit
+
+echo "Installing caelestia-cli"
+pip install --prefix=/usr materialyoucolor
+mkdir -p /tmp/caelestia-cli
+cd /tmp/caelestia-cli
+git clone https://github.com/caelestia-dots/cli.git
+cd cli
+python3 -m build --wheel --no-isolation
+python3 -m installer dist/*.whl --destdir=/
+mkdir -p /usr/share/fish/vendor_completions.d/
+cp completions/caelestia.fish /usr/share/fish/vendor_completions.d/
+cd /
+rm -rf /tmp/caelestia-cli
+
+echo "Installing eza"
 curl -L "https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz" | tar xz -C /tmp
 mv /tmp/eza /usr/bin/eza
 chmod +x /usr/bin/eza
 ln -sf /usr/bin/eza /usr/bin/exa
 eza --version
 
+echo "Installing starship"
 curl -sS https://starship.rs/install.sh | sh -s -- -y -b /usr/bin
 
-dnf5 install -y tmux 
-
+echo "Installing font"s
 FONT_DIR="/usr/share/fonts/jetbrains-nerd"
 mkdir -p "$FONT_DIR"
 wget -P "$FONT_DIR" https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
@@ -86,6 +114,7 @@ rm "$FONT_DIR/JetBrainsMono.zip"
 # Update cache font sistem
 fc-cache -fv
 
+chmod -R a+r /usr/lib/python*/site-packages/
 rpm-ostree cleanup -m
 
 #### Example for enabling a System Unit File
